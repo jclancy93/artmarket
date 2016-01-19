e3caed3fc46aff4c5f111d9687071059a012e0ad
Jack Clancy <jack.clancy93@gmail.com>
2016-01-18 16:48:10 -0800

    added sqlite database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,17 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                        <h2 class="text-center">Search Artworks</h2>
+           <div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="  search-query form-control" placeholder="Search by Artist Name, Fair Name, etc." />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
