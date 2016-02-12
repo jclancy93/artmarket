f880494f212cffee545eed894b0516f5b9c11b35
Jack Clancy <jack.clancy93@gmail.com>
2016-02-11 23:44:38 -0800

    fixing heroku

--- config/app.php
+++ config/app.php
@@ -155,7 +155,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Aws\Laravel\AwsServiceProvider::class,
 
     ],
 
@@ -202,7 +201,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'AWS' => Aws\Laravel\AwsFacade::class,
 
     ],
 
