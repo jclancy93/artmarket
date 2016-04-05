ade0cf0d90d3cb1233e1ab6fb963510ef63c1bb4
Jack Clancy <jack.clancy93@gmail.com>
2016-04-05 00:55:46 -0700

    editable contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app1')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2" style="margin-top: 150px;">  
+            <h1 class="text-center">CONTACT US</h1>
+            <br>
+            <h3 class="text-center"><a href="mailto:art.market.noreply@gmail.com?Subject=Hello" target="_top">artmarket.noreply@gmail.com</a></h3>
+        </div>    
+    </div>
+</div>
+@endsection
