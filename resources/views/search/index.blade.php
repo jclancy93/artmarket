d308c3e3308bcfa5481e3d1bba599786299c5b0f
Jack Clancy <jack.clancy93@gmail.com>
2016-04-05 00:07:25 -0700

    testing

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @if (count($artworks) > 0)
-        <div class="panel panel-default">
+        <div class="panel panel-default" id="desktop">
         <br>
         <br>
         <br>
