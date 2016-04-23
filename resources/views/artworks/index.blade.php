af4e0b92e22d7dc4f4cde9bcabf7b255fee66790
Jack Clancy <jack.clancy93@gmail.com>
2016-04-23 16:22:16 -0700

    mobile artists result page style changes

--- resources/views/artworks/index.blade.php
+++ resources/views/artworks/index.blade.php
@@ -206,7 +206,13 @@
                     <a href="#" target="_parent"></a>
 
                     <h4 class="media-heading">
-                      <a href="#" target="_parent">{{$artwork->artwork or 'Untitled'}}</a>
+                      <a href="#" target="_parent">
+                          @if (is_null($artwork->artwork)) 
+                          {{$artwork->artwork}}
+                          @else 
+                          Untitled
+                          @endif
+                      </a>
                     </h4>
 
                     <ul class="list-inline mrg-0 btm-mrg-10 clr-535353">
